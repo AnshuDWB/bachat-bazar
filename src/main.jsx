1d8e0e555ede30d77592afd6980ef311691bc1b4
AnshuDWB <anshu240712@gmail.com>
2026-09-24 19:57:49 +0530

    fix: add robust global ErrorBoundary and cache recovery system

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import ErrorBoundary from './components/common/ErrorBoundary.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { StoreProvider } from './context/StoreContext.jsx';
 import { CartProvider } from './context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <StoreProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </StoreProvider>
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <StoreProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </StoreProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
